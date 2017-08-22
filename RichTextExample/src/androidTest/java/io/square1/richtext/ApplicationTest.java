0fad742ba41ca9b27280fe3d22e495c4ce44249f
Roberto Prato <roberto.prato@gmail.com>
2017-08-22 09:57:41 +0100

    added some tests

--- RichTextExample/src/androidTest/java/io/square1/richtext/ApplicationTest.java
+++ RichTextExample/src/androidTest/java/io/square1/richtext/ApplicationTest.java
@@ -3,6 +3,12 @@ package io.square1.richtext;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import io.square1.richtextlib.v2.RichTextV2;
+import io.square1.richtextlib.v2.content.DocumentElement;
+import io.square1.richtextlib.v2.content.ImageDocumentElement;
+import io.square1.richtextlib.v2.content.RichDocument;
+import io.square1.richtextlib.v2.content.RichTextDocumentElement;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -10,4 +16,76 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+
+    public void testPlainText() throws Exception {
+
+
+        final String HTML = " Some text is parsed here .\n some more text. Hello what is the content of this ? ";
+
+        RichDocument document = RichTextV2.fromHtml(getContext(), HTML);
+
+        assertEquals(document.getElements().size(), 1);
+
+        RichTextDocumentElement element = (RichTextDocumentElement) document.getElements().get(0);
+
+        assertEquals(HTML.equals(element.contentString()), true);
+
+    }
+
+    public void testHtmlP() throws Exception {
+
+        final String content = " content of  tag ";
+        final String HTML = "<p>"+content+"</p>";
+
+        RichDocument document = RichTextV2.fromHtml(getContext(), HTML);
+
+        assertEquals(document.getElements().size(), 1);
+
+        RichTextDocumentElement element = (RichTextDocumentElement) document.getElements().get(0);
+
+        assertEquals(content.equals(element.contentString()), true);
+
+    }
+
+    public void testHtmlH1() throws Exception {
+
+        final String content = " content of  tag ";
+        final String HTML = "<h1>"+content+"</h1>";
+
+        RichDocument document = RichTextV2.fromHtml(getContext(), HTML);
+
+        assertEquals(document.getElements().size(), 1);
+
+        RichTextDocumentElement element = (RichTextDocumentElement) document.getElements().get(0);
+
+        assertEquals(content.equals(element.contentString()), true);
+
+    }
+
+    public void testHtmlIMG() throws Exception {
+
+        final String IMAGE = "http://www.example.com/myimage.jpg";
+        final String HTML = "<img src=\""+IMAGE+"\"></img>";
+
+        RichDocument document = RichTextV2.fromHtml(getContext(), HTML, new RichTextV2.DefaultStyle(getContext()){
+            @Override
+            public boolean extractImages(){
+                return true;
+            }
+        });
+
+        assertEquals(document.getElements().size(), 1);
+
+        DocumentElement element = document.getElements().get(0);
+        assertEquals(element instanceof ImageDocumentElement, true);
+
+        ImageDocumentElement imageDocumentElement = (ImageDocumentElement)element;
+        assertEquals(imageDocumentElement.getImageURL(), IMAGE);
+
+    }
 }
+
+
+
+
